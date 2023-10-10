92bd7d455452c02824fe604614f860d6519ce2f7
Gaëtan de Villèle <gdevillele@gmail.com>
2023-10-10 16:14:30 +0200

    wip gaetan 2

--- clangFormat/main.go
+++ clangFormat/main.go
@@ -7,21 +7,23 @@ import (
 type ClangFormat struct{}
 
 type CheckFormatOpts struct {
-	dir *Directory
+	// recursive bool
+	// ...
 }
 
-func (m *ClangFormat) CheckFormat(ctx context.Context, opts CheckFormatOpts) (*Container, error) {
+func (m *ClangFormat) CheckFormat(ctx context.Context, dir *Directory, opts CheckFormatOpts) (*Container, error) {
 
 	// get Docker container from hub
 	c := dag.Container().From("gaetan/clang-tools")
 
 	// mount directory into container and make it the working directory
-	c = c.WithMountedDirectory("/workdir", opts.dir)
+	c = c.WithMountedDirectory("/workdir", dir)
 	c = c.WithWorkdir("/workdir")
 
 	// --dry-run: do not apply changes
 	command := "set -e ; set -o pipefail ; find . -maxdepth 1 -regex '^.*\\.\\(cpp\\|hpp\\|c\\|h\\)$' -print0 | xargs -0 clang-format --dry-run --Werror -style=file"
 	c = c.WithExec([]string{"ash", "-c", command})
+	c, err := c.Sync(ctx)
 
-	return c.Sync(ctx)
+	return c, err
 }
